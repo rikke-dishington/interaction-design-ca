953ac720169ec248d39d0481c0b4e1f0f4cb19fd
rikke-dishington <85433495+rikke-dishington@users.noreply.github.com>
2022-10-09 20:41:40 +0200

    Jacket

--- js/data/products.js
+++ js/data/products.js
@@ -3,28 +3,32 @@ export default [
     id: 1,
     name: "Rainy Blue",
     price: "2000,-",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum justo tincidunt, fermentum nisl non, varius nisi. Integer vulputate lectus ut elit bibendum finibus. Sed laoreet aliquet lorem dapibus eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempor arcu, in iaculis enim lacinia nec. Etiam efficitur finibus nulla, ac placerat nisl aliquet eget.",
     image: "/images/product_blue.png",
   },
   {
     id: 2,
     name: "Rainy Green",
     price: "2000,-",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum justo tincidunt, fermentum nisl non, varius nisi. Integer vulputate lectus ut elit bibendum finibus. Sed laoreet aliquet lorem dapibus eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempor arcu, in iaculis enim lacinia nec. Etiam efficitur finibus nulla, ac placerat nisl aliquet eget.",
     image: "/images/product_green.png",
   },
   {
     id: 3,
     name: "Sunny White",
     price: "1750,-",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum justo tincidunt, fermentum nisl non, varius nisi. Integer vulputate lectus ut elit bibendum finibus. Sed laoreet aliquet lorem dapibus eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempor arcu, in iaculis enim lacinia nec. Etiam efficitur finibus nulla, ac placerat nisl aliquet eget.",
     image: "/images/product_white.png",
   },
   {
     id: 4,
     name: "Cloudy Grey",
     price: "1500,-",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum justo tincidunt, fermentum nisl non, varius nisi. Integer vulputate lectus ut elit bibendum finibus. Sed laoreet aliquet lorem dapibus eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempor arcu, in iaculis enim lacinia nec. Etiam efficitur finibus nulla, ac placerat nisl aliquet eget.",
     image: "/images/product_grey.png",
   },
 ];
--- js/jacket-details.js
+++ js/jacket-details.js
@@ -8,12 +8,19 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 const product = products.find((id) => id == id);
+
 console.log(product);
 
-function createHtml(details) {
-  detailContainer.innerHTML = `
-                                <div>${details.description}</div>
-                                <div>${details.price}</div>`;
+function getProduct() {
+  detailContainer.innerHTML += `<div class="product-content">
+                                  <img src="${product.image}"></img>
+                                  <div class="product-details">
+                                    <h2>${product.name}</h2>
+                                    <h3>${product.price}</h3>
+                                    <p>${product.description}</p>
+                                    <button class=cta>Add to cart</button>
+                                  </div>
+                                </div>`;
 }
 
-createHtml();
+getProduct();
